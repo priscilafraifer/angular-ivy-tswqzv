0581819d7660e66e7dba87bc73cf6669eeee692a
priscilafraifer <87189775+priscilafraifer@users.noreply.github.com>
2022-09-02 20:28:35 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  name = 'Priscila Fraifer';
+  nascimento = 1987
 }
