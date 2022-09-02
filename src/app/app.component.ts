5312362fe2ae0dfa38a10f2611faca0a0d4db97f
priscilafraifer <87189775+priscilafraifer@users.noreply.github.com>
2022-09-02 20:39:52 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,25 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Priscila Fraifer';
-  nascimento = 1987
+  nascimento = 1987;
+  counter = 0;
+  timer = setInterval(() => {
+    this.counter = this.counter + 1;
+  }, 100);
+
+  getIdade() {
+    return 2022 - this.nascimento;
+  }
+
+  getMinutos(){
+return Math.trunc(this.counter / 60);
+  }
+
+  getSegundos(){
+    return Math.trunc(this.counter % 60);
+  }
 }
