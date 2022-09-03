41757f5f43c100fa9fc578fb3ba6c2a4ada3b859
priscilafraifer <87189775+priscilafraifer@users.noreply.github.com>
2022-09-02 21:28:03 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,20 @@ export class AppComponent {
   counter = 0;
   timer = setInterval(() => {
     this.counter = this.counter + 1;
-  }, 100);
+  }, 1000);
 
   getIdade() {
     return 2022 - this.nascimento;
   }
 
-  getMinutos(){
-return Math.trunc(this.counter / 60);
+  getMinutos() {
+    return Math.trunc(this.counter / 60);
   }
 
-  getSegundos(){
+  getSegundos() {
     return Math.trunc(this.counter % 60);
   }
-  getNumbers(){
-    return [25,3,7,13,19,35,26,42];
+  getNumbers() {
+    return [25, 3, 7, 13, 19, 35, 26, 42];
   }
 }
